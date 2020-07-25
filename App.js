0a06cc4a476ebb0e8c89f34ed1049c75177f971c
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-07-25 17:38:51 +0900

    :rocket: Finish
    finshi

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Loader from "./Loader";
+import * as Location from "expo-location";
+import { Alert, Text } from "react-native";
+import axios from "axios";
+import Weather from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "c4eee9d8edc069ad19d12fa428a7c27e";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+    temp: 0,
+    condition: "",
+  };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log(data);
+    this.setState({
+      isLoading: false,
+      temp: data.main.temp,
+      condition: data.weather[0].main,
+    });
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad.");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loader />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Weather.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+const weatherOptions = {
+  Clouds: {
+    iconName: "weather-cloudy",
+    colors: ["#c2c2c2", "#323232"],
+  },
+};
+
+export default Weather = ({ temp, condition }) => {
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={weatherOptions[condition].colors}
+    >
+      <StatusBar barStyle="light-content" />
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          name={weatherOptions[condition].iconName}
+          size={84}
+          color="#fff"
+        />
+        <Text style={styles.temp}>{temp} °C</Text>
+      </View>
+      <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+        <Text style={styles.title}>Clouds</Text>
+        <Text style={styles.subtitle}>We are boring</Text>
+      </View>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 32,
+    color: "#fff",
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 44,
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "#fff",
+    fontSize: 20,
+  },
+  textContainer: {
+    alignItems: "flex-start",
+  },
+});
